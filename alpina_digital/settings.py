51f8c6f10cdc1cf6f4a35188fa7e936e72225462
Pilot-333 <den-drupin@yandex.ru>
2025-10-18 10:38:08 +0300

    создал services.py

--- alpina_digital/settings.py
+++ alpina_digital/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'alpina_digital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'bots'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
